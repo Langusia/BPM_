f462d219bfc7e552a7e43cf855830c0b92516957
Guram Elkanishvili <ze156324829@gmail.com>
2024-06-06 15:46:31 +0400

    moved loanRequestAggregate and fixed MoveToWithScan

---
+++ MyCredo/Features/Loan/OtpValidate/OtpValidated.cs
@@ -0,0 +1,4 @@
+﻿using Core.BPM.BCommand;
+
+namespace MyCredo.Retail.Loan.Application.Features.TwoFactor.OtpValidate;
+public record OtpValidated(Guid OtpSessionId, bool ValidOtp) : BpmEvent;
---
+++ MyCredo/Features/Loan/OtpValidate/ValidateOtp.cs
@@ -0,0 +1,21 @@
+﻿using Core.BPM.Application.Managers;
+using Core.BPM.MediatR.Attributes;
+using Credo.Core.Shared.Library;
+using Credo.Core.Shared.Mediator;
+using MyCredo.Common;
+using MyCredo.Retail.Loan.Application.Features.RequestLoanProcess.CarPawnshop;
+
+namespace MyCredo.Retail.Loan.Application.Features.TwoFactor.OtpValidate;
+
+[BpmProducer(typeof(OtpValidated))]
+public record ValidateOtp(Guid ProcessId, int UserId, ChannelTypeEnum Channel, string Otp) : ICommand<AggregateResult<bool>>;
+
+internal class ValidateOtpCommandHandler(
+    BpmManager<OtpValidation> _bpmManager
+) : ICommandHandler<ValidateOtp, AggregateResult<bool>>
+{
+    public async Task<Result<AggregateResult<bool>>> Handle(ValidateOtp request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
