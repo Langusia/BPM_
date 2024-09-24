6784669ad73030a80409174cdfb74eebfca42ef5
Guram Elkanishvili <ze156324829@gmail.com>
2024-09-24 16:07:44 +0400

    updated test clients

---
+++ MyCredo/Features/Loan/LoanV9/Events.cs
@@ -0,0 +1,11 @@
+﻿using Core.BPM.BCommand;
+using MediatR;
+using MyCredo.Common;
+
+namespace MyCredo.Features.Loan.LoanV9;
+
+public record IssueLoanInitiated(int UserId, long ApplicationId, ChannelTypeEnum Channel) : BpmEvent;
+
+public record GeneratedContract(int UserId, ChannelTypeEnum Channel, bool IsHeader, string Language) : BpmEvent;
+
+public record GeneratedSchedule(long CustomerId, ChannelTypeEnum Channel) : BpmEvent, IRequest<bool>;
